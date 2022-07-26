ef8aef8097a3b74e8275b25d8465e4b14b95ac3e
Morra <104777030+MorraMay@users.noreply.github.com>
2022-07-26 15:09:45 +0300

    now

--- js/script.js
+++ js/script.js
@@ -37,6 +37,7 @@ AOS.init({
   $('.menu__btn').click(function(event) {
     $('.header .menu').toggleClass('active');
     $('.menu__btn i').toggleClass('active');
+    $('.menu__logo').toggleClass('active');
   });
    
 
@@ -47,88 +48,6 @@ var typed = new Typed(".typing", {
   typeSpeed: 100,
   backSpeed: 60,
   loop:true
-});
-
-
-
-//Card 3D
-$(".card, .card-2").mousemove(function (e) {
-  var rect = e.target.getBoundingClientRect();
-  var x = e.clientX - rect.left; //x position within the element.
-  var y = e.clientY - rect.top; //y position within the element.
-  var offsetX = x / rect.width;
-  var offsetY = y / rect.height;
-
-  var rotateY = ((offsetX - 0) / (1 - 0)) * (limits - -limits) + -limits;
-  var rotateX = ((offsetY - 0) / (1 - 0)) * (limits - -limits) + -limits;
-
-  var shadowOffsetX = ((offsetX - 0) / (1 - 0)) * (16 - -16) + -16;
-  var shadowOffsetY = ((offsetY - 0) / (1 - 0)) * (16 - -16) + -16;
-
-  $(this).css({
-    "box-shadow":
-      (1 / 6) * shadowOffsetX * -1 +
-      "px " +
-      (1 / 6) * shadowOffsetY * -1 +
-      "px 3px rgba(0, 0, 0, 0.051), " +
-      (2 / 6) * shadowOffsetX * -1 +
-      "px " +
-      (2 / 6) * shadowOffsetY * -1 +
-      "px 7.2px rgba(0, 0, 0, 0.073), " +
-      (3 / 6) * shadowOffsetX * -1 +
-      "px " +
-      (3 / 6) * shadowOffsetY * -1 +
-      "px 13.6px rgba(0, 0, 0, 0.09), " +
-      (4 / 6) * shadowOffsetX * -1 +
-      "px " +
-      (4 / 6) * shadowOffsetY * -1 +
-      "px 24.3px rgba(0, 0, 0, 0.107), " +
-      (5 / 6) * shadowOffsetX * -1 +
-      "px " +
-      (5 / 6) * shadowOffsetY * -1 +
-      "px 45.5px rgba(0, 0, 0, 0.129), " +
-      shadowOffsetX * -1 +
-      "px " +
-      shadowOffsetY * -1 +
-      "px 109px rgba(0, 0, 0, 0.18)",
-    transform:
-      "perspective(1000px) rotateX(" +
-      -rotateX +
-      "deg) rotateY(" +
-      rotateY +
-      "deg)"
-  });
-
-  $(".card").mouseleave(function (e) {
-    $(".card").css({"box-shadow": "0px 0px 3px rgba(0, 0, 0, 0.051), 0px 0px 7.2px rgba(0, 0, 0, 0.073), 0px 0px 13.6px rgba(0, 0, 0, 0.09), 0px 0px 24.3px rgba(0, 0, 0, 0.107), 0px 0px 45.5px rgba(0, 0, 0, 0.129), 0px 0px 109px rgba(0, 0, 0, 0.18)", "transform": "scale(1.0)"});
-  });
-
-  $(".card-2").mouseleave(function (e) {
-    $(".card-2").css({"box-shadow": "0px 0px 3px rgba(0, 0, 0, 0.051), 0px 0px 7.2px rgba(0, 0, 0, 0.073), 0px 0px 13.6px rgba(0, 0, 0, 0.09), 0px 0px 24.3px rgba(0, 0, 0, 0.107), 0px 0px 45.5px rgba(0, 0, 0, 0.129), 0px 0px 109px rgba(0, 0, 0, 0.18)", "transform": "scale(1.0)"});
-  });
-
-});
-
-
-//click and move to element
-$('.menu__link-2').click(function() {
-  var target = $('.content');
-  if (target.length) {
-      $('html,body').animate({
-          scrollTop: target.offset().top
-      }, 1000);
-      return false;
-  }
-});
-
-$('.menu__link-3').click(function() {
-  var target = $('.container__projects');
-  if (target.length) {
-      $('html,body').animate({
-          scrollTop: target.offset().top
-      }, 1000);
-      return false;
-  }
 });
 
 
@@ -274,6 +193,7 @@ var number = document.getElementById('value');
     document.getElementById(tabnames).classList.add("active-tabs");
   }
 
+   //header sticky
   $(document).ready(function(){
     $(window).scroll(function(){
       if(this.scrollY > 20){
